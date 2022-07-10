9b10fe9b624b2fb63144c5a082bb233758e07fa8
Maksym-Semenov <mestcomua@gmail.com>
2022-07-10 18:21:05 +0300

    update

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -11,24 +11,32 @@ namespace HomeWork6
         public static List <int> ReadNumber(int a, int b)
         {
             List<int> list = new List<int>(10);
-            Console.Write("Enter current value:\t");
-            int currentValue = Convert.ToInt32(Console.ReadLine());
+            
             int count = 0;
             try
             {
                 int temp = a;
-        step1:
+            step1:
+                Console.Write("Enter current value:\t");
+                int currentValue = Convert.ToInt32(Console.ReadLine());
+                
                 if (currentValue > a && currentValue < b && currentValue > temp && count < 10)
                 {
-                    list[count] = currentValue;
+                    list.Add(currentValue);
                     count++;
                     temp = currentValue;
                     goto step1;
                 }
-                else if (count >= 10)
+                else if (currentValue < temp)
                 {
-                    goto step2;
+                    throw new Exception("It's wrong value!");
+                    
                 }
+                goto step1;
+                //else if (count >= 10)
+                //{
+                //    goto step2;
+                //}
                 else
                 {
                     throw new Exception("It's wrong value!");
@@ -49,7 +57,7 @@ namespace HomeWork6
             Console.Write("Enter finishing range:\t");
             int finishRange = Convert.ToInt32(Console.ReadLine());
             list = ReadNumber(startRange, finishRange);
-            Console.WriteLine(list);
+            Console.WriteLine(list.ToString());
             Console.ReadLine();
         }
     }
