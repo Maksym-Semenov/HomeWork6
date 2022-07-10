c8fdbd96a9ba5f5bae8913ae795e8ba075949639
Maksym Semenov <mestcomua@gmail.com>
2022-07-10 23:12:14 +0300

    finish

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,63 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork6
 {
     internal class Program
     {
-        public static List <int> ReadNumber(int a, int b)
+        public static List <int> ReadNumber(int startRange, int finishRange)
         {
             List<int> list = new List<int>(10);
             
             int count = 0;
             try
             {
-                int temp = a;
-            step1:
-                Console.Write("Enter current value:\t");
-                int currentValue = Convert.ToInt32(Console.ReadLine());
-                
-                if (currentValue > a && currentValue < b && currentValue > temp && count < 10)
+                int forComparison = startRange;
+            action4:
+                Console.Write($"Enter current value more than {forComparison}:\t");
+                bool successParse = int.TryParse(Console.ReadLine(), out int currentValue);
+                if (successParse)
                 {
-                    list.Add(currentValue);
-                    count++;
-                    temp = currentValue;
-                    goto step1;
+                    if (currentValue > startRange && currentValue < finishRange && currentValue > forComparison && count < 10)
+                    {
+                        list.Add(currentValue);
+                        count++;
+                        forComparison = currentValue;
+                        goto action4;
+                    }
+                    else if (currentValue < startRange || currentValue > finishRange)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your current value is out of range");
+                        Console.ResetColor();
+                        goto action4;
+                    }
+                    else if (currentValue <= forComparison)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Current value must be more than last value");
+                        Console.ResetColor();
+                        goto action4;
+                    }
+                    else if (count >= 10)
+                    {
+                        return list;
+                    }
+                    else
+                    {
+                        throw new Exception("It's wrong value!");
+                    }
                 }
-                else if (currentValue < temp)
-                {
-                    throw new Exception("It's wrong value!");
-                    
-                }
-                goto step1;
-                //else if (count >= 10)
-                //{
-                //    goto step2;
-                //}
                 else
                 {
-                    throw new Exception("It's wrong value!");
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Your type is not right!");
+                    Console.ResetColor();
+                    goto action4;
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-        step2:
             return list;
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("\t HomeWork 6");
+     action1:
+            Console.Write("Enter starting value of range:\t");
+            bool successParse1 = int.TryParse(Console.ReadLine(), out int startRange);
+            if (successParse1)
+            {
+                goto action2;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t Your type is not right!");
+                Console.ResetColor();
+                goto action1;
+            }
+     action2:
+            Console.Write("Enter last value of range:\t");
+            bool successParse2 = int.TryParse(Console.ReadLine(), out int finishRange);
+            if (successParse2)
+            {
+                goto action3;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t Your type is not right!");
+                Console.ResetColor();
+                goto action2;
+            }
+     action3:
             List<int> list = new List<int>(10);
-            Console.Write("Enter starting range:\t");
-            int startRange = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter finishing range:\t");
-            int finishRange = Convert.ToInt32(Console.ReadLine());
             list = ReadNumber(startRange, finishRange);
-            Console.WriteLine(list.ToString());
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("\t Your sequence of values:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadLine();
         }
     }
